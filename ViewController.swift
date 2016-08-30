bc204ce31dd385c74e56120da78f5506a588bdf3
EPON <EPON@EPONteki-Mac-mini.local>
2016-08-30 22:25:47 +0800

    added files for supercool

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Git-Test
+//
+//  Created by EPON on 25/8/2016.
+//  Copyright © 2016年 EPON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
